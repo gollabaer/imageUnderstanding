378852b4e68c8e09235b7dd28db37fec401eebe7
NoNoid <nonoid@gmx-topmail.de>
2015-05-19 17:19:32 +0200

    removed unused function declarations from custHog.hpp

--- include/globalDescriptors/custHog.hpp
+++ include/globalDescriptors/custHog.hpp
@@ -18,13 +18,4 @@ public:
   virtual cv::Mat compute(std::vector<datasetIO::dataItem> items) const;
 };
 
-cv::Mat getRandomHogs(const std::string classname, const unsigned int seed, const int num, datasetIO::dataSet dataset);
-
-void compareHogsOfTwoClasses(const int num, const std::string classname1, const std::string classname2, datasetIO::dataSet dataset, const unsigned int seed);
-
-void compareHogsOfTwoRandomClasses(datasetIO::dataSet dataset, const int numSamples);
-
-void compareHogsOfOneClass(datasetIO::dataSet dataset, std::string className, unsigned int seed);
-
-
 #endif //GLOB_FEAT
